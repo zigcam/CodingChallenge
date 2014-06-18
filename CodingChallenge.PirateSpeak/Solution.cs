6087e6aadd516295715d284f6b601f119f876e5e
cammygriz <campgris@icloud.com>
2014-06-18 01:28:43 -0500

    Solution for Test Cases

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CodingChallenge.PirateSpeak
 {
@@ -7,7 +8,37 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            var wordList = dictionary.ToList();
+            
+
+            for (int i = 0; i < dictionary.Length; i++)
+            {
+                var tempWordList = wordList;
+                var tempJumble = jumble;
+
+                //Remove from the list if the length isn't equal
+                if (dictionary[i].Length != jumble.Length)
+               {
+                   tempWordList.Remove(dictionary[i]);
+                   continue;
+               }
+
+               //remove from the list based on each word in the dictionary (determined by the characters in those words)
+               foreach (var c in dictionary[i])
+               {
+                   var charToRemove = tempJumble.IndexOf(c);
+                   {
+                       if (!tempJumble.Contains(c))
+                       {
+                           tempWordList.Remove(dictionary[i]);
+                           break;
+                       }
+                   }
+                   tempJumble = tempJumble.Remove(charToRemove, 1);               
+                   continue;
+               }
+            }
+            return wordList.ToArray();
         }
     }
 }
